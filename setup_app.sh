27af4c2d6a6075e8666ee1c4a01e9ed361da66c6
GavrilS <gavr1lll@abv.bg>
2025-09-12 08:04:47 +0300

    Added additional dependencies for the project

--- setup_app.sh
+++ setup_app.sh
@@ -6,6 +6,9 @@ python3 -m venv ./env
 # Enable the environment
 source env/bin/activate
 
+# Add MariadDB Connector/Python dependencies
+sudo apt-get install libmariadb3 libmariadb-dev
+
 # Install the required python dependencies for the application
 python3 -m pip install -r requirements.txt
 
