3d62af4c8f7bbea40c16626f41a036a2a112f39b
GavrilS <gavr1lll@abv.bg>
2025-09-14 16:25:10 +0300

    Added the skeleton for the query manager class

---
+++ flaskr/database/query_manager.py
@@ -0,0 +1,34 @@
+class QueryManager:
+    '''
+    This class is responsible for providing the relevant queries for db operations. Main db operations:
+        - add table entry
+        - update table entry
+        - remove table entry
+        - get table entry
+    '''
+
+    def __init__(self):
+        '''
+        Empty constructor as no attributes are required for the manager to work.
+        '''
+        pass
+
+    def insert(self):
+        '''
+        Builds a db insert query with the relevant information.
+        '''
+
+    def update(self):
+        '''
+        Builds a db update query.
+        '''
+
+    def remove(self):
+        '''
+        Builds a db remove query.
+        '''
+    
+    def get(self):
+        '''
+        Builds a db get query.
+        '''
