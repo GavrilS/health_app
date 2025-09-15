d37d31163cdb56c1dc3af8a306133937e7a67187
GavrilS <gavr1lll@abv.bg>
2025-09-15 21:39:44 +0300

    Added the code for the update a table record and refactored duplicate code into a separate method

--- flaskr/database/query_manager.py
+++ flaskr/database/query_manager.py
@@ -20,12 +20,7 @@ class QueryManager:
         '''
         query = None
         try:
-            fields = args[0]
-            values = args[1]
-            table_name = args[2]
-
-            if len(fields) != len(values):
-                raise Exception('Fields and values parameters do not match...')
+            fields, values, table_name = self._parse_modify_table_args(args)
             
             insert_start_statement = f'INSERT INTO {table_name} (FIELDS_PLACEHOLDER) '
             insert_end_statement = 'VALUES (VALUES_PLACEHOLDER);'
@@ -44,8 +39,37 @@ class QueryManager:
 
     def make_update_query(self, *args, **kwargs):
         '''
-        Builds a db update query.
+        Builds a db update query with the relevant information included in the args/kwargs parameters.
+        *args: ([field_list], [value_list], table_name, [where_clause_fields], [where_clause_values])
         '''
+        query = None
+        try:
+            fields, values, table_name = self._parse_modify_table_args(args)
+
+            where_fields = args[3]
+            where_values = args[4]
+            if len(where_fields) != len(where_values):
+                raise Exception('Fields and values parameters for the WHERE clause do not match...')
+
+            update_statement = f'UPDATE {table_name} SET VALUES_PLACEHOLDER WHERE WHERE_CLAUSE_PLACEHOLDER;'
+            values_string = ''
+            where_clause_string = ''
+
+            for i in range(len(fields)):
+                values_string += f'{fields[i]} = {values[i]}'
+                if i < len(fields) - 1:
+                    values_string += ', '
+            
+            for i in range(len(where_fields)):
+                where_clause_string += f'{where_fields[i]}={where_values[i]}'
+                if i < len(where_fields):
+                    where_clause_string += ' AND '
+            
+            query = update_statement.replace('VALUES_PLACEHOLDER', values_string).replace('WHERE_CLAUSE_PLACEHOLDER', where_clause_string)
+        except Exception as e:
+            print(f"Couldn't build the update query due to an error - {str(e)}")
+
+        return query
 
     def make_remove_query(self, *args, **kwargs):
         '''
@@ -56,3 +80,13 @@ class QueryManager:
         '''
         Builds a db get query.
         '''
+
+    def _parse_modify_table_args(self, *args):
+        fields = args[0]
+        values = args[1]
+        table_name = args[2]
+
+        if len(fields) != len(values):
+            raise Exception('Fields and values parameters do not match...')
+
+        return fields, values, table_name
