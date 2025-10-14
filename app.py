2e8ce0b66ec8a09f02ed80bc0f85dd68994020b7
GavrilS <gavr1lll@abv.bg>
2025-10-14 21:29:52 +0300

    Added the basic route skeleton for the app

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ def main():
     app = f.create_app()
     db_manager = OperationManager()
 
+    app.run(debug=True)
+
 
 if __name__=='__main__':
     main()
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,4 +4,32 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    @app.route('/')
+    def index():
+        return 'Hello World!'
+    
+    @app.route('/nutrition')
+    def show_nutrition_category():
+        return 'Nutrition'
+    
+    @app.route('/nutrition/<article_id>')
+    def show_nutrition_article(article_id):
+        return f'Opening nutrition article with id {article_id}'
+    
+    @app.route('/physical')
+    def show_physical_category():
+        return 'Physical'
+    
+    @app.route('/physical/<article_id>')
+    def show_physical_article(article_id):
+        return f'Opening physical article with id {article_id}'
+    
+    @app.route('/mind')
+    def show_mind_category():
+        return 'Mind'
+    
+    @app.route('/mind/<article_id>')
+    def show_mind_article(article_id):
+        return f'Opening mind article with id {article_id}'
+
     return app
