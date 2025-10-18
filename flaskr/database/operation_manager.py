051a3c8eb509c49a4566155671ab8fbe0f5c7a21
GavrilS <gavr1lll@abv.bg>
2025-10-18 11:41:42 +0300

    Added the method to execute db queries and return the result to the OperationManager class

--- flaskr/database/operation_manager.py
+++ flaskr/database/operation_manager.py
@@ -1,7 +1,6 @@
 import os
 import mariadb
-from flaskr.database.query_manager import QueryManager
-# from flaskr.database.model_manager import ModelManager
+
 
 class OperationManager:
     '''
@@ -11,20 +10,10 @@ class OperationManager:
         - delete
         - get
     '''
-
-    # def __init__(self, query_manager=QueryManager(), model_manager=ModelManager()):
-    #     self.query_manager = query_manager
-    #     # self.model_manager = model_manager
-
-    def __init__(self, query_manager=QueryManager()):
-        self._query_manager = query_manager
+    def __init__(self):
         self._conn = None
-
-    @property
-    def query_manager(self):
-        return self._query_manager
     
-    def set_db_connection(self):
+    def _set_db_connection(self):
         try:
             self._conn = mariadb.connect(
                 user=os.environ.get('USER', None),
@@ -47,4 +36,8 @@ class OperationManager:
             print(f"Couldn't close the DB connection: {str(e)}")
             raise
 
-    
+    def execute_query(self, query):
+        self._set_db_connection()
+        cur = self._conn.cursor()
+        cur.execture()
+        return cur
