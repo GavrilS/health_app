a0a6b5525db17d4907d666147eebec187bafd1fe
GavrilS <gavr1lll@abv.bg>
2025-12-07 22:05:19 +0200

    Fixed 'Create new article' button to show hidden form

---
+++ flaskr/static/app.js
@@ -0,0 +1,6 @@
+const newArticleForm = document.getElementById('create-article');
+const createArticleBtn = document.getElementById('new-article');
+
+createArticleBtn.addEventListener('click', () => {
+    newArticleForm.style.display = 'block';
+});
