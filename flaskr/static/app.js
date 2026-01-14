4674aa3ef65f038d47548d9c389aa77de50e4494
GavrilS <gavr1lll@abv.bg>
2026-01-14 09:16:07 +0200

    Adding the current path in the site controls

--- flaskr/static/app.js
+++ flaskr/static/app.js
@@ -4,3 +4,19 @@ const createArticleBtn = document.getElementById('new-article');
 createArticleBtn.addEventListener('click', () => {
     newArticleForm.style.display = 'block';
 });
+
+function getUrlPath() {
+    let path = window.location.pathname;
+    if (path.startsWith("/")) {
+        path = path.substring(1);
+    }
+    let pathArray = path.split("/");
+    return pathArray;
+}
+
+function createUrlPath() {
+    let pathArray = getUrlPath()
+    pathArray.forEach(item => {
+        
+    })
+}
